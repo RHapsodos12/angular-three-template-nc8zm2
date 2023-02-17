8f4cddfc53f298f7bc87efa24cd7f16f83bc4b7b
RHapsodos12 <davidsantacruz09@gmail.com>
2023-02-17 10:29:41 -0600

    1 file modified

--- src/app/engine/engine.service.ts
+++ src/app/engine/engine.service.ts
@@ -1,18 +1,55 @@
 import * as THREE from 'three';
 import { Injectable, ElementRef, OnDestroy, NgZone } from '@angular/core';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import {
+  DirectionalLight,
+  HemisphereLight,
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer,
+} from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 @Injectable({ providedIn: 'root' })
 export class EngineService implements OnDestroy {
   private canvas: HTMLCanvasElement;
-  private renderer: THREE.WebGLRenderer;
-  private camera: THREE.PerspectiveCamera;
-  private scene: THREE.Scene;
+
   private light: THREE.AmbientLight;
 
   private cube: THREE.Mesh;
-  
 
   private frameId: number = null;
+
+  aspect: number;
+  camera: PerspectiveCamera;
+  controls: OrbitControls;
+  hemisphere: HemisphereLight;
+  loader: GLTFLoader;
+  mainLight: DirectionalLight;
+  scene: Scene;
+
+  deltaX = 0.01;
+  deltaY = 0.01;
+  deltaZ = 0.01;
+  far = 100;
+  fov = 35;
+  gammaFactor = 2.2;
+  gammaOutput = true;
+  near = 1;
+  physicallyCorrectLights = true;
+  sceneBackground = 0x8fbcd4;
+  renderer = new WebGLRenderer({ antialias: true });
+
+  directionalLightOptions = {
+    color: 0xffffff,
+    intensity: 5,
+  };
+
+  hemisphereOptions = {
+    skyColor: 0xddeeff,
+    groundColor: 0x0f0e0d,
+    intensity: 5,
+  };
 
   public constructor(private ngZone: NgZone) {}
 
@@ -28,8 +65,8 @@ export class EngineService implements OnDestroy {
 
     this.renderer = new THREE.WebGLRenderer({
       canvas: this.canvas,
-      alpha: true,    // transparent background
-      antialias: true // smooth edges
+      alpha: true, // transparent background
+      antialias: true, // smooth edges
     });
     this.renderer.setSize(window.innerWidth, window.innerHeight);
 
@@ -37,21 +74,39 @@ export class EngineService implements OnDestroy {
     this.scene = new THREE.Scene();
 
     this.camera = new THREE.PerspectiveCamera(
-      75, window.innerWidth / window.innerHeight, 0.1, 1000
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
     );
     this.camera.position.z = 5;
     this.scene.add(this.camera);
 
     // soft white light
-    this.light = new THREE.AmbientLight( 0x404040 );
+    this.light = new THREE.AmbientLight(0x404040);
     this.light.position.z = 10;
     this.scene.add(this.light);
 
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    this.cube = new THREE.Mesh( geometry, material );
-    this.scene.add(this.cube);
+    const loader = new GLTFLoader();
 
+    let modelUrl =
+      'https://gateway.pinata.cloud/ipfs/QmY6B4Z65YBVF8VWDgMGJFV1dFzomRXDWkwpm8A5uRw7kp?_gl=1*1qg9xi*_ga*NzM4MTUyNzAxLjE2NzYzOTc2OTc.*_ga_5RMPXG14TE*MTY3NjY1MDYwNi41LjAuMTY3NjY1MDYwOS41Ny4wLjA.';
+
+    loader.load(
+      modelUrl,
+      (gltf) => {
+        this.scene.add(gltf.scene);
+      },
+      undefined,
+      function (error) {
+        console.error(error);
+      }
+    );
+
+    // const geometry = new THREE.BoxGeometry(1, 1, 1);
+    // const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    // this.cube = new THREE.Mesh(geometry, material);
+    // this.scene.add(this.cube);
   }
 
   public animate(): void {
@@ -77,8 +132,8 @@ export class EngineService implements OnDestroy {
       this.render();
     });
 
-    this.cube.rotation.x += 0.01;
-    this.cube.rotation.y += 0.01;
+    // this.cube.rotation.x += 0.01;
+    // this.cube.rotation.y += 0.01;
     this.renderer.render(this.scene, this.camera);
   }
 
@@ -89,6 +144,6 @@ export class EngineService implements OnDestroy {
     this.camera.aspect = width / height;
     this.camera.updateProjectionMatrix();
 
-    this.renderer.setSize( width, height );
+    this.renderer.setSize(width, height);
   }
 }
